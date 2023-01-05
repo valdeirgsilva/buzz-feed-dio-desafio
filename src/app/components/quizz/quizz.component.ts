abe693716e6b71716e0b9bb002846f90bea89fb0
valdeirgsilva <valdeirgabriels@gmail.com>
2023-01-04 22:22:37 -0300

    Criando as propriedades lógicas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
+  title: string = "";
+
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answrSelected: string = "";
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
